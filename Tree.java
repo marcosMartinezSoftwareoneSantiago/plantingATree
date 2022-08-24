0a394e2e054d1a3dab89b32dd19d03732e62b9a0
marcosMartinezSoftwareoneSantiago <Marcos.Martinez@Softwareone.com>
2022-08-24 19:17:04 +0200

    first commit

---
+++ Tree.java
@@ -0,0 +1,21 @@
+// Define your tree class in this file!
+public class Tree{
+    public static void main(String[] args){
+      //it will works
+      System.out.println("Hey there, I'm Marcos and I'm learning to code in Java!");
+      System.out.println("I’m going to plant a tree today!");
+      System.out.println("  *  ");
+      System.out.println(" *** ");
+      System.out.println(" *** ");
+      System.out.println("  *  ");
+      System.out.println("  *  ");  
+  
+      System.out.println("I’m going to plant another tree today!");
+      System.out.println(" * ** * ");
+      System.out.println(" ****** ");
+      System.out.println("  ****  ");
+      System.out.println("   **   ");
+      System.out.println("   **   "); 
+      System.out.println("~~~~~~~~~~");
+    }
+  }
